22696133be2c23527697146992477ba087290584
Drew Mettlach <dmmettlach@gmail.com>
2021-01-07 04:03:14 +0000

    Add methods to build closure sets and initial closure set

--- set_generator.hpp
+++ set_generator.hpp
@@ -2,18 +2,132 @@
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
+#include <queue>
 #include <regex>
+
+// special symbol for grammar rule serparator
+const static std::string RULE_SEP = "->";
+
+// empty set symbol
+const static std::string EPSILON = "~";
+
+// Determines if symbol is a terminal
+// TODO : Find a better spot for this
+bool is_terminal(const std::string symbol) {
+  return !std::regex_match(symbol, std::regex("[A-Z]"));
+}
+
+// Remove space characters in string
+// TODO : Find a better spot for this
+std::string remove_whitespace(const std::string& s) {
+  std::string ret = "";
+  for(const char& c : s) {
+    if(c == ' ') {
+      continue;
+    }
+
+    ret += c;
+  }
+
+  return ret;
+}
 
 class LR1Item {
   public:
     LR1Item(std::string production, std::string lookahead, int position) :
-      production(production),
       lookahead(lookahead),
-      position(position) {};
+      position(position) {
+        // Split production at "->" into LHS and RHS
+        const auto found = production.find("->");
+        if(found == std::string::npos) {
+          throw std::runtime_error("Invalid LR1Item production: " + production);
+        }
+
+        // Given S -> A, found = 2
+        // lhs = "S "
+        // rhs = " A"
+        lhs = production.substr(0, found);
+        rhs = production.substr(found + 2, std::string::npos);
+
+        // Trim leading and trailing whitespace from both strings
+        lhs = remove_whitespace(lhs);
+        rhs = remove_whitespace(rhs);
+      };
+
+    // Returns true if the symbol to the right of position is
+    // a non-terminal
+    // S -> . E returns true
+    // S -> . "(" returns false
+    bool next_is_non_terminal() const {
+      if(position >= rhs.size()) {
+        return false;
+      }
+
+      return !is_terminal(rhs.substr(position, 1));
+    }
+
+    // Gets the symbol to the right of position
+    // on the RHS
+    // S -> A . B returns B
+    // S -> A . returns empty string
+    std::string get_next_symbol() const {
+      if(position < rhs.size()) {
+        return rhs.substr(position, 1);
+      }
+
+      return "";
+    }
+
+    // Given item [A → α ⋅ B β, t], returns β
+    // S -> A . B C returns [C]
+    std::string get_beta_symbols() const {
+      if(position >= rhs.size() - 1) {
+        return "";
+      }
+
+      return rhs.substr(position + 1, std::string::npos);
+    }
+
+    // Gets the lookahead token
+    std::string get_lookahead() const {
+      return lookahead;
+    }
+
+    // Returns a string of the members that can be used
+    // for hashing
+    std::string get_str_for_hash() const {
+      return lhs+rhs+lookahead+std::to_string(position);
+    }
+
+    // Move the position marker to the right
+    // S -> A . B becomes S -> A B .
+    void increment_position() {
+      if(position < rhs.size()) {
+        ++position;
+      }
+    }
+
+    bool operator==(const LR1Item& rhs) const {
+      return this->get_str_for_hash() == rhs.get_str_for_hash();
+    }
+
+    // Returns string version of this item
+    // i.e S->A.B,$
+    std::string to_string() const {
+      std::string rhs_with_pos = rhs;
+      rhs_with_pos.insert(position, ".");
+
+      return lhs + RULE_SEP + rhs_with_pos + "," + lookahead;
+    }
 
   private:
-    // Production without lookahead like S -> E
-    std::string production;
+    // LHS of production
+    // With S -> E, lhs = S
+    std::string lhs;
+
+    // RHS of production
+    // With S -> E, rhs = E
+    std::string rhs;
 
     // Terminal lookahead 
     std::string lookahead;
@@ -21,6 +135,13 @@ class LR1Item {
     // The current position of the marker
     // E.g. when production = S -> E and position = 0 then LR item =  S -> . E
     int position;
+};
+
+struct LR1ItemHash {
+  // Return a hash for this item
+  std::size_t operator()(const LR1Item& item) const noexcept {
+    return std::hash<std::string>{}(item.get_str_for_hash());
+  }
 };
 
 class SetGenerator {
@@ -33,7 +154,7 @@ class SetGenerator {
 
       for(const std::string& production : grammar) {
         const std::string symbol = production.substr(0, 1);
-        first(grammar, symbol);
+        first_of(grammar, symbol);
       }
 
       // We don't need empty set key in firsts sets
@@ -42,13 +163,69 @@ class SetGenerator {
       return first_sets;
     }
 
+    /**
+     * Builds the closure set for the items in s
+     * 
+     * closure(S)
+     * For each item [A → α ⋅ B β, t] in S,
+     *   For each production B → γ in G,
+     *     For each token b in FIRST(βt),
+     *       Add [B → ⋅ γ, b] to S
+     */
+    std::unordered_set<LR1Item, LR1ItemHash> build_closure_set(const std::vector<std::string>& grammar, const std::unordered_set<LR1Item, LR1ItemHash>& s) {
+      std::queue<LR1Item> q;
+
+      for(const LR1Item& item : s) {
+        q.push(item);
+      }
+
+      std::unordered_set<LR1Item, LR1ItemHash> closure;
+
+      // For each item [A → α ⋅ B β, t] in S
+      while(!q.empty()) {
+        const LR1Item& item = q.front(); q.pop();
+
+        if(item.next_is_non_terminal()) {
+          // For each production B → γ in G
+          std::string B = item.get_next_symbol();
+          std::vector<std::string> productions = get_symbol_productions(grammar, B);
+          for(const std::string production : productions) {
+            // For each token b in FIRST(βt)
+            std::string beta = item.get_beta_symbols();
+            std::string t = item.get_lookahead();
+            
+            // For each token b in FIRST(βt)
+            std::unordered_set<std::string> first_tokens = first(beta+t);
+            for(std::string b : first_tokens) {
+              LR1Item closure_item(production, b, 0);
+              // Add [B → ⋅ γ, b] to S
+              auto result = closure.insert(closure_item);
+
+              if(result.second) {
+                q.push(closure_item);
+              }
+            }
+          }
+        }
+      }
+
+      return closure;
+    }
+
+    // Augments grammar then builds closure from the augmented item
+    std::unordered_set<LR1Item, LR1ItemHash> build_initial_closure(const std::vector<std::string>& grammar) {
+      // Create set with initial item
+      LR1Item augmented("S' -> E", "$", 0);
+      std::unordered_set<LR1Item, LR1ItemHash> s;
+      s.insert(augmented);
+
+      std::unordered_set<LR1Item, LR1ItemHash> closure = build_closure_set(grammar, s);
+      s.merge(closure);
+
+      return s;
+    }
+
   private:
-    // special symbol for grammar rule serparator
-    const std::string RULE_SEP = "->";
-
-    // empty set symbol
-    const std::string EPSILON = "~";
-
     std::unordered_map<std::string, std::unordered_set<std::string>> first_sets;
 
   private:
@@ -67,7 +244,7 @@ class SetGenerator {
      *     - If First(Y1) First(Y2)..First(Yk) all contain ε then add ε
      *       to First(Y1Y2..Yk) as well.
      */
-    void first(const std::vector<std::string>& grammar, const std::string& symbol) {
+    void first_of(const std::vector<std::string>& grammar, const std::string& symbol) {
 
       if(first_sets.count(symbol) > 0) {
         return;
@@ -96,7 +273,7 @@ class SetGenerator {
           if(cur_symbol == " ") {
             continue;
           }
-          first(grammar, cur_symbol);
+          first_of(grammar, cur_symbol);
 
           std::unordered_set<std::string> y = first_sets[cur_symbol];
 
@@ -122,6 +299,51 @@ class SetGenerator {
       }
     }
 
+    /**
+     * Compute the first set for a string of symbols
+     * 
+     * Rules taken from Dragon 4.4.2:
+     * 
+     * Add to FIRST(X1X2...Xn) all non-epsilon symbols of FIRST(X1)
+     * Also add all non-epsilon symbols of FIRST(X2) if epsilon in FIRST(X1),
+     * the non-epsilon symbols of FIRST(X3) if epsilon in FIRST(X1) and FIRST(X2).
+     * Finally add epsilon to FIRST(X1X2...Xn) if epsilon in FIRST(Xi), 1 <= i <= n
+     */
+    std::unordered_set<std::string> first(const std::string& symbols) {
+      std::unordered_set<std::string> first_set;
+
+      bool last_had_epsilon = true;
+      for(int i = 0; i < symbols.size(); ++i) {
+        // Get Xi from symbols
+        std::string symbol = symbols.substr(i, 1);
+
+        // Do nothing if symbol isn't in first_sets table
+        if(symbol == EPSILON || first_sets.count(symbol) == 0) {
+          continue;
+        }
+
+        // Merge FIRST(symbol) which may include epsilon
+        first_set.merge(first_sets[symbol]);
+
+        if(first_set.count(EPSILON) > 0) {
+          // Remove epsilon and continue to next symbol
+          first_set.erase(EPSILON);
+        }
+        else {
+          // Epsilon not in FIRST(symbol) so we are done
+          last_had_epsilon = false;
+          break;
+        }
+      }
+
+      // epsilon in FIRST(Xi), 1 <= i <= n so add it to to FIRST(X1X2...Xn)
+      if(last_had_epsilon) {
+        first_set.insert(EPSILON);
+      }
+
+      return first_set;
+    }
+
     // Given a production like A -> sSB, returns sSB
     std::string get_RHS(const std::string production) {
       const auto& found = production.find(RULE_SEP);
@@ -131,11 +353,6 @@ class SetGenerator {
       }
 
       return "";
-    }
-
-    // Determines if symbol is a terminal
-    bool is_terminal(const std::string symbol) {
-      return !std::regex_match(symbol, std::regex("[A-Z]"));
     }
 
     /**
@@ -158,5 +375,30 @@ class SetGenerator {
       }
 
       return ret;
+    }
+
+    // Looks up each symbol in beta + t in first_sets
+    // then returns the merged set
+    // Returns FIRST(βt)
+    std::unordered_set<std::string> get_first_tokens(const std::string& beta, const std::string& t) {
+      std::unordered_set<std::string> first_tokens;
+
+      // Get first_set of each symbol in beta
+      for(int i = 0; i < beta.size(); ++i) {
+        std::string symbol = beta.substr(i, 1);
+        
+        if(first_sets.count(symbol) == 0) {
+          continue;
+        }
+
+        first_tokens.merge(first_sets[symbol]);
+      }
+
+      // Merge first set of lookahead t
+      if(first_sets.count(t) > 0) {
+        first_tokens.merge(first_sets[t]);
+      }
+
+      return first_tokens;
     }
 };
